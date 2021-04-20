3dc5b916f1c8ae544a7a9ee3c312ffc6715654c3
oniya24 <24320172203177@stu.xmu.edu.cn>
2021-04-20 18:03:56 +0800

    feat: 修改前缀

--- src/const/router.tsx
+++ src/const/router.tsx
@@ -1,5 +1,5 @@
 export const nologRoutes: Array<string> = ['/login', '/register', '/404'];
 export const BASEURL =
   process.env.NODE_ENV == 'production'
-    ? 'http://47.96.155.159:8083'
-    : 'http://47.96.155.159:8083';
+    ? 'http://101.200.49.56:8083'
+    : 'http://101.200.49.56:8083';
