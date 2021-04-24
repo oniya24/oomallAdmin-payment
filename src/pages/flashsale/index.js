76c3829dd69843060e58e7854dfa35ae7f95fbc7
oniya24 <24320172203177@stu.xmu.edu.cn>
2021-04-24 16:10:07 +0800

    feat: 增加两个新的活动

---
+++ src/pages/flashsale/index.js
@@ -0,0 +1,91 @@
+import { useMemo, useEffect, useRef, useState } from 'react';
+import { connect } from 'umi';
+import {
+  Card,
+  Table,
+  Button,
+  Tooltip,
+  Space,
+  Form,
+  DatePicker,
+  Modal,
+  Input,
+} from 'antd';
+
+const presale = () => {
+  const presaleList = [
+    {
+      id: 1,
+      skuId: '247',
+      startTime: '2021-04-15 19:59:31',
+      createTime: '2021-04-14 19:59:31',
+      state: 1,
+    },
+    {
+      id: 2,
+      skuId: '248',
+      startTime: '2021-04-15 19:59:31',
+      createTime: '2021-04-14 19:59:31',
+      state: 0,
+    },
+  ];
+  const columns = useMemo(() => {
+    return [
+      {
+        title: 'id',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: '秒杀商品skuId',
+        dataIndex: 'skuId',
+        key: 'skuId',
+      },
+      {
+        title: '秒杀时间',
+        dataIndex: 'startTime',
+        key: 'startTime',
+      },
+      {
+        title: '创建时间',
+        dataIndex: 'createTime',
+        key: 'createTime',
+      },
+      {
+        title: '操作',
+        key: 'operation',
+        dataIndex: 'operation',
+        render: (text, record) => {
+          const { state } = record;
+          return (
+            <Space>
+              {Number(state) === 0 ? (
+                <Button type="primary">上架活动</Button>
+              ) : (
+                <Button type="primary">下架活动</Button>
+              )}
+              <Button type="default">修改活动信息</Button>
+              <Button type="danger">删除活动</Button>
+            </Space>
+          );
+        },
+      },
+    ];
+  }, []);
+  return (
+    <Card>
+      <div style={{ margin: 10 }}>
+        <Button type="primary">创建预售活动</Button>
+      </div>
+      <Table
+        scroll={{ x: true }}
+        // pagination={pagination(shareTotal, saveAdverPagination)}
+        rowKey={record => record.dataIndex}
+        columns={columns}
+        dataSource={presaleList}
+      ></Table>
+    </Card>
+  );
+};
+
+export default presale;
